f48ce32c92e6f7f29f30c96b124dacd85d6d31b2
Andrew Alsing <andrew32alsing@gmail.com>
2024-01-16 14:46:27 -0800

    finished stretch challenges for challenge 13

--- src/Product.js
+++ src/Product.js
@@ -1,11 +1,21 @@
 import React from "react";
 
 function Product(props) {
+  const units = props.units;
+  const rating = Math.round(props.rating);
+
   return (
-    <div className="product">
+    <div className={`product ${units === 0 ? "disabled" : ""}`}>
       <h1>{props.name}</h1>
-      <p>{props.price}</p>
-      <p>{props.category}</p>
+      <p>Category: {props.category}</p>
+      <p>Price: ${parseFloat(props.price.slice(1)).toFixed(2)}</p>
+      <p>Units: {units}</p>
+      <p>
+        Rating:{" "}
+        {Array.from({ length: rating }, (_, i) => (
+          <span key={i}>★</span>
+        ))}
+      </p>
     </div>
   );
 }
